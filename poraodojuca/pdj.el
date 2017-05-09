183f2ba530cc4741810d2994208941e066dbf75e
Juca Crispim <juca@poraodojuca.net>
2017-05-08 23:34:49 -0300

    better initialization

--- poraodojuca/pdj.el
+++ poraodojuca/pdj.el
@@ -20,6 +20,10 @@
 
 (defun pdj:--do-setup ()
 
+  (add-to-list 'load-path "~/.emacs.d/poraodojuca")
+  (add-to-list 'load-path "~/.emacs.d/third")
+  (add-to-list 'load-path "~/.emacs.d/third/multi-term-20150220.1320-pdj/")
+
   (require 'pdj-common)
   (require 'pdj-python)
   (require 'pdj-elisp)
